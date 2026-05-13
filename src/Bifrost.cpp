87f17bd99c490d3448676c6a3ed6f780ca0cc160
Bhoomi-newbie <ipmangrove@gmail.com>
2026-05-13 17:10:11 +0530

    totp implementation added

--- src/Bifrost.cpp
+++ src/Bifrost.cpp
@@ -9,13 +9,15 @@ int main(){
     Bytes key;
     time_t timestamp = time(NULL);
     cpp_int timestep = timestamp/30;
-    Bytes message= Diffie_Hellman::cpp_int_to_bytes(timestep);
-    Bytes hmac_bytes= hmac_sha1(&key, &message);
-    int offset= hmac_bytes[hmac_bytes.size-1] & 0x0F;
+    Bytes message = Diffie_Hellman::cpp_int_to_bytes(timestep);
+    Bytes hmac_bytes = hmac_sha1(&key, &message);
+    int offset= hmac_bytes[hmac_bytes.size()-1] & 0x0F;
     Bytes sample_bytes;
-    for(int i=offset;i<offset+5;i++) sample_bytes.push_back(hmac_bytes[offset]);
-    
-
+    for(int i=offset;i<offset+4;i++) sample_bytes.push_back(hmac_bytes[i]);
+    sample_bytes[0]&=0x7f;
+    cpp_int sample = bytesToCppInt(sample_bytes);
+    int OTP = (sample%1000000).convert_to<int>();
+    return OTP;
 
 
 
